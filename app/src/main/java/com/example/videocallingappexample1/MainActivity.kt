1e653361ee314e0f263ffdd517c9ae4c119c8473
Rontu22 <rontubarhoi01@gmail.com>
2021-06-25 23:24:59 +0530

    Video calling is working in kotlin.

--- app/src/main/java/com/example/videocallingappexample1/MainActivity.kt
+++ app/src/main/java/com/example/videocallingappexample1/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.videocallingappexample1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import org.jitsi.meet.sdk.JitsiMeetActivity
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        try {
+            var options = JitsiMeetConferenceOptions.Builder()
+                .setServerURL(URL(""))
+                .setWelcomePageEnabled(false)
+                .build()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Toast.makeText(this,e.message,Toast.LENGTH_LONG).show()
+        }
+
+
+    }
+
+    fun onJoinButtonClick(view: View) {
+        val editTextJoinMeeting = findViewById<EditText>(R.id.conferenceName)
+        val roomName = editTextJoinMeeting.text.toString()
+
+        if(roomName.length > 0)
+        {
+            val options = JitsiMeetConferenceOptions.Builder()
+                .setRoom(roomName)
+                .build()
+
+            JitsiMeetActivity.launch(this,options)
+        }
     }
 }
